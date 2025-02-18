30b5927b2e80d4d43ef3c964b5cb4595723bf843
Shreya Pandey <130491256+shreyapandeeyy@users.noreply.github.com>
2025-02-18 17:00:44 +0530

    Update interntaskhtop.py

--- interntaskhtop.py
+++ interntaskhtop.py
@@ -3,17 +3,20 @@ import os
 import subprocess
 from datetime import datetime
 import pytz
+import getpass
 
 app = Flask(__name__)
 
 @app.route('/htop')
 def htop():
     full_name = "Shreya Rajesh Pandey"
-    username = os.getlogin()
+    username = getpass.getuser()
     ist = pytz.timezone('Asia/Kolkata')
     server_time = datetime.now(ist).strftime('%Y-%m-%d %H:%M:%S')
     top_output = subprocess.check_output(['top', '-b', '-n', '1']).decode()
+    gitlink=""
     return f"Name - {full_name}<br>Username - {username}<br>Server Time in IST - {server_time}<br><pre>{top_output}</pre>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+                            
